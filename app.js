30f67b0f5db4e989539180454434800bd8d1dbad
ณัฐชา แหละปานแก้ว <natcha-lpk@hotmail.com>
2025-09-02 12:15:54 +0700

    fix for test

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const express = require('express');
-const app = express();
-const { add } = require('./math');
+function sum(a, b) {
+    return a + b;
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// route ใหม่
-app.get('/sum/:a/:b', (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  res.send(`Sum = ${add(a, b)}`);
-});
-
-app.listen(3000, () => {
-  console.log('App running on port 3000');
-});
+module.exports = { sum };
--- math.test.js
+++
@@ -1,5 +0,0 @@
-const { add } = require('./math');
-
-test('add 2 + 3 = 5', () => {
-  expect(add(2, 3)).toBe(5);
-});
