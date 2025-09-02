ab173b167fabf4394d531860d7f26ca18aeafea8
ณัฐชา แหละปานแก้ว <natcha-lpk@hotmail.com>
2025-09-02 12:39:07 +0700

    Add small bug/code smell for SonarQube test

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = { sum };
+function unusedFunction() { // Code Smell: function ไม่ถูกเรียก
+    console.log("This function is never called");
+}
+
+function multiply(a, b) {
+    return a + b; // Bug: logic ผิด (จริง ๆ ต้อง a * b)
+}
+
+module.exports = { sum, multiply };
